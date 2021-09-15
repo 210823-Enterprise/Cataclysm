d89b8559102a906107c46365c1e75f14646452c2
Ty Johnson <tdj182@gmail.com>
2021-09-14 21:14:15 -0500

    Base annotations setup

---
+++ src/main/java/com/revature/util/Configuration.java
@@ -0,0 +1,46 @@
+package com.revature.util;
+
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Configuration {
+	
+	// all jdbc properties needed to establish a connection with the database
+	
+	private String dbUrl;
+	private String username;
+	private String password;
+	private List<MetaModel<Class<?>>> metaModelList;
+	
+	// this essentially does what the Hibernate.cfg.xml mapping property does!
+	public Configuration addAnnotatedClass(Class annotatedClass) {
+		
+		if (metaModelList == null) {
+			
+			metaModelList = new LinkedList<>();
+			
+		}
+		
+		metaModelList.add(MetaModel.of(annotatedClass)); // we will amke this of() method
+		
+		// Create the of() method inside MetaModel to trasform a class
+		// into an appropraite data model to be transposed into a relational db object.
+		
+		return this;
+	}
+	
+	
+	public List<MetaModel<Class<?>>> getMetaModels() {
+		
+		return (metaModelList == null) ? Collections.emptyList() : metaModelList;
+		
+	}
+	
+	
+
+}
+
+
+
+
