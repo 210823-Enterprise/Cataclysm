b4aaf3070697936e12e0fe77248e9b9b897a522b
jmliguid <joshualiguid@gmail.com>
2021-09-20 16:35:05 -0700

    All my stuff

--- src/main/java/com/revature/customStatement/Operators.java
+++ src/main/java/com/revature/customStatement/Operators.java
@@ -1,5 +1,5 @@
 package com.revature.customStatement;
 
 public enum Operators {
-
+	EQUAL, NOT_EQUAL, GREATER_THAN, LESS_THAN, GREATER_THAN_OR_EQUAL, LESS_THAN_OR_EQUAL, BETWEEN, LIKE, IN, IS, IS_NOT, UNARY
 }
--- src/main/java/com/revature/customStatement/Where.java
+++ src/main/java/com/revature/customStatement/Where.java
@@ -1,5 +1,14 @@
 package com.revature.customStatement;
 
 public class Where {
+	
+	public  String name;
+	public Class valueDataType;
+	public String value = "";
+	public Class value2dataType;
+	public String value2 = null;
+	public final static String NULL = "NULL";
+	public Conditionals conditional;
+	public Operators operator;
 
 }
