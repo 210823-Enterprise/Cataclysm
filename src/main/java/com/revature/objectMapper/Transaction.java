818410ba9e9cc46e1394337ef12b76ade49e2e7a
jmliguid <joshualiguid@gmail.com>
2021-09-24 16:11:50 -0700

    Transactions saved.

--- src/main/java/com/revature/objectMapper/Transaction.java
+++ src/main/java/com/revature/objectMapper/Transaction.java
@@ -20,50 +20,96 @@ public class Transaction {
 		savepoints = new HashMap<>();
 	}
 	
+	public Transaction getTransaction() {
+		return tr;
+	}
 	
-	public void enableAutoCommit(Connection conn) {
+	
+	public boolean enableAutoCommit(Connection conn) {
 		try {
 			conn.setAutoCommit(true);
 			log.info("Auto-commit enabled.");
+			return true;
 		} catch (SQLException e) {
 			log.error("Auto-commit was not able to be set to true.");
+			return false;
 		}
 	}
 	
-	public void disableAutoCommit(Connection conn) {
+	public boolean disableAutoCommit(Connection conn) {
 		try {
 			conn.setAutoCommit(false);
 			log.info("Auto-commit disabled.");
+			return true;
 		} catch (SQLException e) {
 			log.error("Auto-commit was not able to be set to false.");
+			return false;
 		}
 	}
 	
-	public void commit(Connection conn) {
+	public boolean commit(Connection conn) {
 		try {
 			conn.commit();
 			log.info("Committed transaction.");
+			return true;
 		} catch (SQLException e) {
 			log.error("Committing transaction was not able to be done.");
+			return false;
 		}
 	}
 	
-	public void rollack(Connection conn) {
+	public boolean rollack(Connection conn) {
 		try {
 			conn.rollback();
 			log.info("Rollback committed.");
+			return true;
 		} catch (SQLException e) {
 			log.error("Rollback failed.");
+			return false;
 		}
 	}
 	
-	public void Savepoint(String name, Connection conn) {
+	public boolean Savepoint(String name, Connection conn) {
 		try {
 			Savepoint save = conn.setSavepoint(name);
 			savepoints.put(name, save);
 			log.info("Initiated save point for " + save);
+			return true;
 		} catch (SQLException e) {
 			log.error("Savepoint initialization failed.");
+			return false;
+		}
+	}
+	
+	public boolean ReleaseSavepoint(final String name, final Connection conn) {
+		try {
+			if (savepoints.containsKey(name)) {
+				conn.releaseSavepoint(savepoints.get(name));
+				log.info("Save point " + name + " removed.");
+				return true;
+			} else {
+				log.warn("Trying to remove a non-existent savepoint");
+				return false;
+			}
+		} catch (SQLException e) {
+			log.error("Failed to remove save point: ", e);
+			return false;
+		}
+	}
+	
+	public boolean rollbackSavepoint(final String name, final Connection conn) {
+		try {
+			if (savepoints.containsKey(name)) {
+				conn.rollback(savepoints.get(name));
+				log.info("Rolled back to your savepoint.");
+				return true;
+			} else {
+				log.warn("Savepoint does not exist.");
+				return false;
+			}
+		} catch (SQLException e) {
+			log.error("Error in rolling back transaction: ",e);
+			return false;
 		}
 	}
 
